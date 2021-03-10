19decccf44322640db166d5bd7081a02c778d00d
thomasnguyen <imtoshiru@gmail.com>
2021-03-09 22:44:53 -0800

    Back button

--- barkboard/components/Header.js
+++ barkboard/components/Header.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 import styled from "styled-components";
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
 const HeaderContainer = styled.div`
   display: flex;
@@ -10,16 +12,50 @@ const HeaderContainer = styled.div`
   margin-bottom: 20px;
   h1 {
     font-weight: 100;
+    cursor: pointer;
+  }
+
+  .right {
+    opacity: 0;
   }
 `;
 
+const BackButton = styled.div`
+  position: absolute;
+  left: 20px;
+  top: 30px;
+  color: white;
+`;
+
 export default function Header({ ...props }) {
+  const router = useRouter();
+
+  const goBack = () => {
+    const path = window.document.location.pathname.split("/");
+
+    // remove last
+    path.pop();
+
+    const newPath = path.join("/");
+
+    router.push("/");
+  };
+
+  const showBackButton = window.document.location.pathname !== "/";
+
   return (
     <>
+      {showBackButton && (
+        <BackButton className="left" onClick={goBack}>
+          <KeyboardBackspaceIcon />
+        </BackButton>
+      )}
       <HeaderContainer>
-        <h1>
-          <strong>Bark</strong>Board
-        </h1>
+        <Link href="/">
+          <h1>
+            <strong>Bark</strong>Board
+          </h1>
+        </Link>
       </HeaderContainer>
     </>
   );
--- barkboard/components/SoundKey.js
+++ barkboard/components/SoundKey.js
@@ -2,11 +2,22 @@ import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import useSound from "use-sound";
 
+const Button = styled.button`
+  border-radius: 20px;
+  border: 2px solid white;
+  background: none;
+  color: white;
+  outline: 0;
+  padding: 10px 20px;
+  margin: 10px;
+  font-size: 16px;
+`;
+
 export default function SoundKey(props) {
   const [play] = useSound(props.sound.audioFile, {
     interrupt: true,
     volume: props.volume * 0.01,
   });
 
-  return <button onClick={() => play()}>{props.sound.label}!</button>;
+  return <Button onClick={() => play()}>{props.sound.label}!</Button>;
 }
